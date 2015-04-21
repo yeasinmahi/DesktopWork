b008a94f85fd648aecba4d94654eb9ba980e0f92
yeasinmahi <Yeasinmahi72@yahoo.com>
2015-04-21 17:17:38 +0600

    DB ADD

---
+++ dbEmployeeInfoSave/dbEmployeeInfoSave/BLL/EmpManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dbEmployeeInfoSave.Model;
+
+namespace dbEmployeeInfoSave.BLL
+{
+    class EmpManager
+    {
+        dbConnector connector = new dbConnector();
+        public string Insert(EmployeeInfo employee)
+        {
+            if (connector.InsertData(employee))
+            {
+                return "Saved Successfuly";
+            }
+            else
+            {
+                return "A problem Occured in insert";
+            }
+            
+        }
+    }
+}
---
+++ dbEmployeeInfoSave/dbEmployeeInfoSave/DAL/dbConnector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using dbEmployeeInfoSave.Model;
+
+namespace dbEmployeeInfoSave
+{
+    class dbConnector
+    {
+        private string connectionString = @"server=.\SQLEXPRESS; database=employee; integrated security=true;";
+
+        public bool InsertData(EmployeeInfo emp)
+        {
+            string query = "insert into [emp] ([name],[email],[address],[salary]) values ('"+emp.name+"','"+emp.email+"','"+emp.address+"',"+emp.salary+")";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(query,connection);
+            connection.Open();
+            int check=command.ExecuteNonQuery();
+            connection.Close();
+            if (check>0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
