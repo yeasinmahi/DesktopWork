cb5940845cc70fe743130eeb7b6a1723fec18c9c
yeasinmahi <yeasinmahi72@gmail.com>
2015-05-28 02:26:55 +0600

    FareManagment Add

---
+++ FareManagement/FareManagement/UI/MenuUi.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace FareManagement.UI
+{
+    partial class MenuUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuUi));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.visitorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visitorEntryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoneTypeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoneSpecificDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoneWiseVisitorNumberToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 18F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visitorToolStripMenuItem,
+            this.setupToolStripMenuItem,
+            this.reportToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(823, 40);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // visitorToolStripMenuItem
+            // 
+            this.visitorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visitorEntryToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.visitorToolStripMenuItem.Name = "visitorToolStripMenuItem";
+            this.visitorToolStripMenuItem.Size = new System.Drawing.Size(94, 36);
+            this.visitorToolStripMenuItem.Text = "Visitor";
+            // 
+            // visitorEntryToolStripMenuItem
+            // 
+            this.visitorEntryToolStripMenuItem.Name = "visitorEntryToolStripMenuItem";
+            this.visitorEntryToolStripMenuItem.Size = new System.Drawing.Size(218, 36);
+            this.visitorEntryToolStripMenuItem.Text = "Visitor Entry";
+            this.visitorEntryToolStripMenuItem.Click += new System.EventHandler(this.visitorEntryToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(218, 36);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // setupToolStripMenuItem
+            // 
+            this.setupToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoneTypeToolStripMenuItem});
+            this.setupToolStripMenuItem.Name = "setupToolStripMenuItem";
+            this.setupToolStripMenuItem.Size = new System.Drawing.Size(89, 36);
+            this.setupToolStripMenuItem.Text = "Setup";
+            // 
+            // zoneTypeToolStripMenuItem
+            // 
+            this.zoneTypeToolStripMenuItem.Name = "zoneTypeToolStripMenuItem";
+            this.zoneTypeToolStripMenuItem.Size = new System.Drawing.Size(204, 36);
+            this.zoneTypeToolStripMenuItem.Text = "Zone Type";
+            this.zoneTypeToolStripMenuItem.Click += new System.EventHandler(this.zoneTypeToolStripMenuItem_Click);
+            // 
+            // reportToolStripMenuItem
+            // 
+            this.reportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoneSpecificDetailsToolStripMenuItem,
+            this.zoneWiseVisitorNumberToolStripMenuItem});
+            this.reportToolStripMenuItem.Name = "reportToolStripMenuItem";
+            this.reportToolStripMenuItem.Size = new System.Drawing.Size(98, 36);
+            this.reportToolStripMenuItem.Text = "Report";
+            // 
+            // zoneSpecificDetailsToolStripMenuItem
+            // 
+            this.zoneSpecificDetailsToolStripMenuItem.Name = "zoneSpecificDetailsToolStripMenuItem";
+            this.zoneSpecificDetailsToolStripMenuItem.Size = new System.Drawing.Size(387, 36);
+            this.zoneSpecificDetailsToolStripMenuItem.Text = "Zone Specific Visitor Details";
+            this.zoneSpecificDetailsToolStripMenuItem.Click += new System.EventHandler(this.zoneSpecificDetailsToolStripMenuItem_Click);
+            // 
+            // zoneWiseVisitorNumberToolStripMenuItem
+            // 
+            this.zoneWiseVisitorNumberToolStripMenuItem.Name = "zoneWiseVisitorNumberToolStripMenuItem";
+            this.zoneWiseVisitorNumberToolStripMenuItem.Size = new System.Drawing.Size(387, 36);
+            this.zoneWiseVisitorNumberToolStripMenuItem.Text = "Zone Wise Visitor Number";
+            this.zoneWiseVisitorNumberToolStripMenuItem.Click += new System.EventHandler(this.zoneWiseVisitorNumberToolStripMenuItem_Click);
+            // 
+            // dLabel
+            // 
+            this.dLabel.AutoSize = true;
+            this.dLabel.BackColor = System.Drawing.Color.Transparent;
+            this.dLabel.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.dLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 256F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dLabel.ForeColor = System.Drawing.Color.White;
+            this.dLabel.Location = new System.Drawing.Point(127, 40);
+            this.dLabel.Name = "dLabel";
+            this.dLabel.Size = new System.Drawing.Size(353, 387);
+            this.dLabel.TabIndex = 1;
+            this.dLabel.Text = "d";
+            // 
+            // MenuUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(147)))), ((int)(((byte)(58)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(823, 561);
+            this.Controls.Add(this.dLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "MenuUi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fare Management";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MenuUi_Load);
+            this.ResizeEnd += new System.EventHandler(this.MenuUi_ResizeEnd);
+            this.ClientSizeChanged += new System.EventHandler(this.MenuUi_ClientSizeChanged);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem visitorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visitorEntryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoneTypeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoneSpecificDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zoneWiseVisitorNumberToolStripMenuItem;
+        private System.Windows.Forms.Label dLabel;
+    }
+}
---
+++ FareManagement/FareManagement/UI/MenuUi.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FareManagement.UI
+{
+    public partial class MenuUi : Form
+    {
+        public MenuUi()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuUi_Load(object sender, EventArgs e)
+        {
+            SetLabelPosition();  
+        }
+
+        private void visitorEntryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VisitorEntryUi visitorEntryUi = new VisitorEntryUi();
+            visitorEntryUi.Show();
+        }
+
+        private void zoneTypeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ZoneTypeUi zoneTypeUi = new ZoneTypeUi();
+            zoneTypeUi.Show();
+        }
+
+        private void zoneSpecificDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReportOfZoneSpecificVisitor reportOfZoneSpecificVisitor = new ReportOfZoneSpecificVisitor();
+            reportOfZoneSpecificVisitor.Show();
+        }
+
+        private void zoneWiseVisitorNumberToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReportOfZoneWiseVisitorNumber reportOfZoneWiseVisitorNumber = new ReportOfZoneWiseVisitorNumber();
+            reportOfZoneWiseVisitorNumber.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MenuUi_ResizeEnd(object sender, EventArgs e)
+        {
+            SetLabelPosition();
+        }
+
+        private void SetLabelPosition()
+        {
+            int screenWidth = ClientRectangle.Width;
+            int screenHeight = ClientRectangle.Height;
+            int labelHeight = dLabel.Height;
+            int labelwidth = dLabel.Width;
+            dLabel.Location = new Point((screenWidth - labelwidth) / 2, (screenHeight - labelHeight) / 2);
+        }
+
+        private void MenuUi_ClientSizeChanged(object sender, EventArgs e)
+        {
+            SetLabelPosition();
+        }
+    }
+}
